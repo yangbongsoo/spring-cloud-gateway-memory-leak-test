7c3e8e4c5c5ec3c0517a2378680efc2cf6ef105b
양봉수[PaaS] <bongsoo.yang@navercorp.com>
2020-04-28 13:23:37 +0900

    add target server

---
+++ src/main/java/requestDownstream/RequestHandler.java
@@ -0,0 +1,114 @@
+package requestDownstream;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RequestHandler implements Runnable {
+
+	private final static Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	private Socket connection;
+
+	public RequestHandler(Socket connection) {
+		this.connection = connection;
+	}
+
+	@Override
+	public void run() {
+		log.debug("New client! Connected IP : {}, port : {}", connection.getInetAddress(), connection.getPort());
+		OutputStream out = null;
+		InputStream in = null;
+		try {
+			in = connection.getInputStream();
+			out = connection.getOutputStream();
+
+			// request
+			byte[] byteArr = new byte[10000];
+			int readByteCount = in.read(byteArr);
+			String data = new String(byteArr, 0, readByteCount, "UTF-8");
+			log.info("request : {}", data);
+
+
+			// response
+			DataOutputStream dos = new DataOutputStream(out);
+			byte[] body = "Stable Server Response Message".getBytes();
+//			response400Header(dos, body.length);
+			response200Header(dos, body.length);
+			responseBody(dos, body);
+		} catch (IOException e) {
+		} finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+	}
+
+	//	@Override
+	//	public void run() {
+	//		log.debug("New client! Connected IP : {}, port : {}",connection.getInetAddress(), connection.getPort());
+	//		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+	//			DataOutputStream dos = new DataOutputStream(out);
+	//			byte[] body = "Hello World".getBytes();
+	//			response200Header(dos, body.length);
+	//			responseBody(dos, body);
+	//		} catch (IOException e) {
+	//			e.printStackTrace();
+	//		}
+	//	}
+
+	private void response400Header(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 400 BAD REQUEST \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void response500Header(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 400 INTERNEL SERVER ERROR \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void responseBody(DataOutputStream dos, byte[] body) {
+		try {
+			dos.write(body, 0, body.length);
+			dos.writeBytes("\r\n");
+			dos.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
